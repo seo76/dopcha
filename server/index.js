dc7a6137498fce3c7fc4e8a4f0c840ec070d2af9
seo76 <rnmrnrnmrnm@gmail.com>
2021-11-11 20:22:30 +0900

    로그인부분 포스트맨 완..근데 저 이길은 아닌 것 같아요

--- server/index.js
+++ server/index.js
@@ -35,64 +35,67 @@ app.use(bodyParser.json());
       })
     })
 
-// //로그인
-// const cookieParser = require('cookie-parser');
-// const { request } = require('express');
-// app.use(cookieParser());
-// app.post('/login',(req, res)=>{
-//   //요청된 이메일이 데이터베이스에 있는지 
-//   Agency.findOne({email:req.body.email},(err,user)=>{
-//     if(!agency){
-//       return res.json({
-//         loginSuccess: false,
-//         message:"제공된 이메일에 해당하는 유저가 없습니다."
-//       })
-//     }
-//   //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인
-//     agency.comparePassword(req.body.password,(err,isMatch)=>{
-//       if(!isMatch)
-//         return res.json({loginSuccess: false, message:"비밀번호가 틀렸습니다." })
-//       //비밀번호가 같다면 토큰 생성
-//       agency.generateToken((err,agency)=>{
-//         if(err) return res.status(400).send(err);
-//         // 토큰을 cookie 에 저장-
-//         res.cookie("x_auth", agency.token)
-//           .status(200)
-//           .json({loginSuccess: true, agencyrId: agency._id})
+//로그인
+const cookieParser = require('cookie-parser');
+const { request } = require('express');
+app.use(cookieParser());
+app.post('/api/login',(req, res)=>{
+  //요청된 이메일이 데이터베이스에 있는지 
+  Agency.findOne({id:req.body.id},(err,agency)=>{
+    if(!agency){
+      return res.json({
+        loginSuccess: false,
+        message:"제공된 아이디에 해당하는 유저가 없습니다."
+      })
+    }
+  //요청된 아이디가 데이터베이스에 있다면 비밀번호가 맞는지 확인
+    agency.comparePassword(req.body.password,(err,isMatch)=>{
+      if(!isMatch)
+        return res.json({loginSuccess: false, message:"비밀번호가 틀렸습니다." })
+      //비밀번호가 같다면 토큰 생성
+      agency.generateToken((err,agency)=>{
+        if(err) return res.status(400).send(err);
+        // 토큰을 cookie 에 저장-
+        res.cookie("x_auth", agency.token)
+          .status(200)
+          .json({loginSuccess: true, agencyrId: agency._id})
 
-//       })
-//     })
-//   })
-// })
-// //인증
+      })
+    })
+  })
+})
+//인증
 
-// app.get('/api/age/auth', auth ,(req, res)=>{
+app.get('/api/age/auth', auth ,(req, res)=>{
 
-//   //여기까지 미들웨어 통과해왔다 = 인증이 참이라는 말.
-//   res.status(200).json({
-//     _id: req.agency._id,
-//     isAdmin: req.agency.role === 0? false:true,
-//     isAtuth: true,
-//     email: req.agency.email,
-//     name:req.agency.agencyName,
-//     ceoName: req.agency.ceoName,
-//     phone:req.agency.phoe,
-//     fax:req.agency.fax,
-//     file:req.agency.file,
-//     role: req.agency.role,
-//     image: req.agency.image
-//   })
-// })
+  //여기까지 미들웨어 통과해왔다 = 인증이 참이라는 말.
+  res.status(200).json({
+    _id: req.agency._id,
+    isAdmin: req.agency.role === 0? false:true,
+    isAtuth: true,
+    id: req.agency.id,
+    email: req.agency.email,
+    agencyName:req.agency.agencyName,
+    ceoName: req.agency.ceoName,
+    phone:req.agency.phoe,
+    fax:req.agency.fax,
+    website:req.agency.website,
+    file:req.agency.file,
+    role: req.agency.role,
+    image: req.agency.image
+  })
+})
 
-// app.get('/api/users/logout', auth, (req, res)=>{
-//   User.findOneAndUpdate({_id: req.user._id},
-//     {token:""}
-//     ,(err,user)=>{
-//       if(err) return res.json({success:false, err});
-//       return res.status(200).send({
-//         success: true
-//       })
-//     })
+app.get('/api/users/logout', auth, (req, res)=>{
+  User.findOneAndUpdate({_id: req.user._id},
+    {token:""}
+    ,(err,user)=>{
+      if(err) return res.json({success:false, err});
+      return res.status(200).send({
+        success: true
+      })
+    }) 
+  })
 
 
 // })
--- server/models/Agency.js
+++ server/models/Agency.js
@@ -67,64 +67,64 @@ const agencySchema = mongoose.Schema({
     }
 })
 
-// agencySchema.pre('save',function(next){
-//     var agency = this; 
-//     // 비밀번호 암호화
-//     if(agency.isModified('password')){
-//         bcrypt.genSalt(saltRounds, function(err,salt){
-//             if (err) return next(err)
-//             bcrypt.hash (agency.password, salt, function(err,hash){
-//                 if (err) return next(err)
-//                 agency.password = hash
-//                 next()
-//             })
-//         })    
-//     } else{
-//         next()
-//     }
-// })
-// //로그인 시 비밀번호 암호화 후 디비에 저장된 비밀번호와 비교        
-// agencySchema.methods.comparePassword = function(plainPassword, cb){
-//     //plainPassword 1234567 암호화된 비밀번호
-//     bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-//         if(err) return cb(err);
-//             cb(null, isMatch)
-//     })
-// }
-// //로그인 시 토큰 생성
-// agencySchema.methods.generateToken = function(cb){
-//     var agency = this;
-//     var token = jwt.sign(agency._id.toHexString(), 'secretToken')
-//     //user._id + 'secretToken' = token
-//     //->
-//     //'secretToken' -> user._id
-//     agency.token = token
-//     agency.save(function(err,user){
-//         if(err) return cb(err)
-//         cb(null, user)
-//     })
-// }
+agencySchema.pre('save',function(next){
+    var agency = this; 
+    // 비밀번호 암호화
+    if(agency.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err,salt){
+            if (err) return next(err)
+            bcrypt.hash (agency.password, salt, function(err,hash){
+                if (err) return next(err)
+                agency.password = hash
+                next()
+            })
+        })    
+    } else{
+        next()
+    }
+})
+//로그인 시 비밀번호 암호화 후 디비에 저장된 비밀번호와 비교        
+agencySchema.methods.comparePassword = function(plainPassword, cb){
+    //plainPassword 1234567 암호화된 비밀번호
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+            cb(null, isMatch)
+    })
+}
+//로그인 시 토큰 생성
+agencySchema.methods.generateToken = function(cb){
+    var agency = this;
+    var token = jwt.sign(agency._id.toHexString(), 'secretToken')
+    //user._id + 'secretToken' = token
+    //->
+    //'secretToken' -> user._id
+    agency.token = token
+    agency.save(function(err,agency){
+        if(err) return cb(err)
+        cb(null, agency)
+    })
+}
 
-// agencySchema.statics.findByToken = function(token, cb){
-//     var agency = this;
-//     var agency = this;
+agencySchema.statics.findByToken = function(token, cb){
+    var agency = this;
+    var agency = this;
 
-//     //토큰 디코딩
-//     jwt.verify(token, 'secretToken', function(err, decoded){
-//         //유저 아이디를 이용해 유저를 찾은 후
-//         //클라이언트의 토큰과 db의 토큰이 일치하는지 확인
+    //토큰 디코딩
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        //유저 아이디를 이용해 유저를 찾은 후
+        //클라이언트의 토큰과 db의 토큰이 일치하는지 확인
 
-//         agency.findOne({"_id":decoded,"token":token}, function(err,user){
+        agency.findOne({"_id":decoded,"token":token}, function(err,agency){
 
-//             if (err) return cb(err);
-//             cb(null, user)
+            if (err) return cb(err);
+            cb(null, agency)
             
-//         })
+        })
             
-//     })
+    })
 
 
-// }
+ }
 
 const Agency = mongoose.model('Agency', agencySchema)
 
