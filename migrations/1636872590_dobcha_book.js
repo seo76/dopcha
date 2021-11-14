f677f21e867b7b961734292aa50d0c81c84b2bdb
RyuJeongHa <fbwjdgk00@bu.ac.kr>
2021-11-14 15:58:15 +0900

    와이라노.....이게 머선...

---
+++ migrations/1636872590_dobcha_book.js
@@ -0,0 +1,7 @@
+var Dobcha_book = artifacts.require("Dobcha_book");
+
+
+module.exports = function(deployer) {
+  let ownerAddress = web3.eth.accounts[0];
+  deployer.deploy(Dobcha_book, ownerAddress);
+};
