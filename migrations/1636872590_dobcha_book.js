d0e618394fee5f116cab3f6d195f65b0f36267fa
RyuJeongHa <fbwjdgk00@bu.ac.kr>
2021-11-14 15:58:56 +0900

    help

--- migrations/1636872590_dobcha_book.js
+++ migrations/1636872590_dobcha_book.js
@@ -1,6 +1,5 @@
 var Dobcha_book = artifacts.require("Dobcha_book");
 
-
 module.exports = function(deployer) {
   let ownerAddress = web3.eth.accounts[0];
   deployer.deploy(Dobcha_book, ownerAddress);
