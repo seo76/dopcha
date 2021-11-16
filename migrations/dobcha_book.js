184dceeaa13c02e2f7a2882e5863ded5bfc8d444
seo76 <rnmrnrnmrnm@gmail.com>
2021-11-16 20:04:31 +0900

    스마트컨트랙트한테 차였어요

---
+++ migrations/dobcha_book.js
@@ -0,0 +1,8 @@
+var Dobcha_book = artifacts.require("Dobcha_book");
+
+module.exports = function(deployer) {
+  let ownerAddress = web3.eth.accounts[0];
+  let agencyAddress = web3.eth.accounts[9];
+  deployer.deploy(Dobcha_book, ownerAddress, agencyAddress);
+};
+
